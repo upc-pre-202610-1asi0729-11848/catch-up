215dfe943e9d6a7059daa1734e8e91a7e1040de9
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 12:39:28 -0500

    feat(sources): implement source-assembler for transforming source resources to entities.

---
+++ src/app/news/infrastructure/source-assembler.ts
@@ -0,0 +1,26 @@
+import {LogoDevApi} from '../../shared/infrastructure/logo-dev-api';
+import {SourceResource, SourcesResponse} from './sources-response';
+import {Source} from '../domain/model/source.entity';
+
+export class SourceAssembler {
+  static logoApi: LogoDevApi;
+
+  static withLogoApi(logoApi: LogoDevApi) {
+    this.logoApi = logoApi;
+    return this;
+  }
+
+  static toEntityFromResource(resource: SourceResource): Source {
+    return {
+      id: resource.id,
+      name: resource.name,
+      url: resource.url || '',
+      urlToLogo: this.logoApi.getUrlToLogo(resource.url)
+    };
+  }
+
+  static toEntitiesFromResponse(response: SourcesResponse): Source[] {
+    return response.sources.map(source => this.toEntityFromResource(source));
+  }
+
+}
