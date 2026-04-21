20ecace819fefa2c614d07769dcbbf380f9ecda7
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 12:05:54 -0500

    chore(environment): add development environment configuration and file replacements.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: '',
+  logoProviderApiBaseUrl: 'https://img.logo.dev/',
+  logoProviderPublishableKey: ''
+};
