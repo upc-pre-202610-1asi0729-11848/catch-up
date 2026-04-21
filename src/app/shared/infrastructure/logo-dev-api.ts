3ed22f9c263e1f9515d8e38273852dc4fa9b2389
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 12:11:54 -0500

    feat(logo): implement logo url generation through logo-dev api service.

--- src/app/shared/infrastructure/logo-dev-api.ts
+++ src/app/shared/infrastructure/logo-dev-api.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({providedIn: 'root'})
 export class LogoDevApi {
+  baseUrl = environment.logoProviderApiBaseUrl;
+  apiKey  = environment.logoProviderPublishableKey;
 
+  constructor() {}
+
+  getUrlToLogo(domain: string): string {
+    return `${this.baseUrl}${new URL(domain).hostname}?token=${this.apiKey}`;
+  }
 }
