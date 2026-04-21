6ebcb73d8c795369f0332f6fb6f8df714d2df25b
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-21 11:54:12 -0500

    feat(ui-layout): integrate app-layout component into main application.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component, signal } from '@angular/core';
+import {Layout} from './shared/presentation/components/layout/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [
+    Layout
+  ],
   styleUrl: './app.css'
 })
 export class App {
